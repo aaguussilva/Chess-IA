fcf17ea2be491dc04bab959348bf1e838013a613
Agu S <silvaf.agus@gmail.com>
2021-12-06 22:31:39 -0300

    Draw board and pieces

--- ChessEngine.py
+++ ChessEngine.py
@@ -5,6 +5,9 @@ It will also be rsponsible for determining the valid moves at the current state.
 
 class GameState():
     def __init__(self):
+        # Board is a 8x8 2s list, where each element has 2 characters..
+        # The first character represent the color of the piece. "b or w"
+        # The second character reprensent the type of the piece, "R N B Q K"
         self.board = [
             ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
             ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
@@ -15,3 +18,5 @@ class GameState():
             ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
             ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"],
         ]
+        self.WhiteToMove = True
+        self.MoveLog = []
--- ChessMain.py
+++ ChessMain.py
@@ -2,3 +2,62 @@
 This is our main driver file. It will resposible for handling user input and displaying the current Game State Object
 """
 
+from os import name
+import pygame as p
+from pygame import color
+from pygame import draw
+import ChessEngine
+
+WIDTH = HEIGTH = 512
+DIMENSION = 8
+SQ_SIZE = HEIGTH // DIMENSION
+MAX_FPS = 15 # Mas adelante para las animaciones
+IMAGES = {}
+
+def loadImages():
+    pieces = ['bB', 'bK', 'bN', 'bp', 'bQ', 'bR', 'wB', 'wK', 'wN', 'wp', 'wQ', 'wR']
+    for piece in pieces:
+        IMAGES[piece] = p.transform.scale(p.image.load("images/" + piece + ".png"), (SQ_SIZE,SQ_SIZE))
+
+def main():
+    p.init()
+    screen = p.display.set_mode((WIDTH,HEIGTH))
+    clock = p.time.Clock()
+    screen.fill(p.Color("white"))
+    gs = ChessEngine.GameState()  
+    loadImages() # Only we do this once, before the while loop
+    running = True
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+
+        drawGameState(screen,gs)
+        clock.tick(MAX_FPS)
+        p.display.flip()
+
+'''
+Draw the board and the pieces using GameState.board
+'''
+def drawGameState(screen, gs):  
+    drawBoard(screen)
+    drawPieces(screen, gs.board)
+
+def drawBoard(screen):
+    colors = [p.Color("white"), p.Color("grey")]
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            color = colors[((r + c) % 2)]
+            p.draw.rect(screen, color, p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+def drawPieces(screen, board):
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            piece = board[r][c]
+            if piece != "--":
+                screen.blit(IMAGES[piece], p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))  #Draw the image to the screen at the given position
+
+if __name__ == "__main__":
+    main()
+
+
